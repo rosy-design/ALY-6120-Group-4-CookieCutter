a1dff2db98e742e55b0a3b4966dd1b31c20e43b6
rosy <pineda.r.417@gmail.com>
2025-11-11 12:10:47 -0800

    Initial commit with cookiecutter template

---
+++ aly_6120_business_plan/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting customer churn for an online retail company.',
+    author='Group_4',
+    license='MIT',
+)
